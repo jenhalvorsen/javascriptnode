69b93b25b22db45532f9caeac6459d34965596a1
Jens <jensrh@online.no>
2019-10-01 19:26:23 +0200

    Added all files 2 time

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const morgan = require('morgan');
 
+app.use(morgan('dev'));
 app.get('/', (request, response) => response.send('Hello World'));
 
 app.listen(port, function () {
